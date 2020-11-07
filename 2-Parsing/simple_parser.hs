56131c4091331172ebead9680af2a252895be9ad
adriacabeza <adriacabeza@hotmail.com>
2020-11-07 16:34:50 +0100

    adding some of the exercises

--- 2-Parsing/simple_parser.hs
+++ 2-Parsing/simple_parser.hs
@@ -1,55 +1,72 @@
+module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
-import Control.Monad
+import Control.Monad (liftM)
 
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-
 spaces :: Parser ()
 spaces = skipMany1 space
-
 
 readExpr:: String -> String
 readExpr input = case parse parseExpr  "lisp" input of 
 	Left err -> "No match: " ++ show err
 	Right val -> "Found value"
 
-
 main :: IO()
 main = do
 	(expr:_) <- getArgs
 	putStrLn (readExpr expr)
 
-
 parseString :: Parser LispVal
 parseString = do
 		char '"'
-		x <- many (noneOf "\"")
+		x <- many $ escapedChars <|> (noneOf ['\\', '"'])
 		char '"'
 		return $ String x
+
+escapedChars :: Parser Char
+escapedChars = do
+				char '\\'
+				c <- oneOf ['\\', '"', 'n', 'r', 't']
+				return $ case c of 
+					'\\' -> c
+					'"' -> c
+					'n' -> '\n'
+					'r' -> '\r'
+					't' -> '\t'
+
 
 parseAtom :: Parser LispVal
 parseAtom = do 
 		first <- letter <|> symbol
-		REST <- many (letter <|> digit <|> symbol)
+		rest <- many (letter <|> digit <|> symbol)
 		let atom = first:rest
 		return $ case atom of
 			"#t" -> Bool True
 			"#f" -> Bool False
 			_ -> Atom atom 
 
-
 parseNumber :: Parser LispVal
-parseNunmber = liftM (Number . read) $ many1 digit
+-- parseNumber = do
+-- 			numbers <- many1 digit
+-- 			return $ (Number . read) $ numbers
+parseNumber = (many1 digit) >>= (\x -> return ((Number . read) x))
 
+parseFloat :: Parser LispVal
+parseFloat = do
+           whole <- many1 digit
+           char '.'
+           decimal <- many1 digit
+           return $ Float (read (whole++"."++decimal) :: Double)
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom 
 	<|> parseString
+	<|> try parseFloat
 	<|> parseNumber
-
 
 
 data LispVal = Atom String
@@ -58,3 +75,5 @@ data LispVal = Atom String
 	| Number Integer
 	| String String
 	| Bool Bool 
+	| Char Char
+	| Float Double
